2116ff65256be7ff39e2a35a947fd3c02bd283b8
AdithyaVimukthi <85384599+AdithyaVimukthi@users.noreply.github.com>
2022-09-15 02:23:32 +0530

    gui_002

--- GUI.py
+++ GUI.py
@@ -10,7 +10,15 @@ import cv2
 from PIL import Image, ImageTk
 
 
-def reset_counter():
+def start():
+    pass
+
+
+def end():
+    pass
+
+
+def pause():
     pass
 
 
@@ -19,52 +27,72 @@ window.geometry("960x540")
 window.title("Human Hand Tracking System")
 ck.set_appearance_mode("dark")
 
-# lables
+#lables
+tilable = ck.CTkLabel(window, height=40, width=320, text_font=("Calibre", 20), text_color="white", padx=10, fg_color="blue")
+tilable.place(x=280, y=10)
+tilable.configure(text='Human Motion Recognition System')
+
+'''
 classLabel = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="black", padx=10)
 classLabel.place(x=10, y=1)
 classLabel.configure(text='STAGE')
 
-counterLabel = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="black", padx=10)
-counterLabel.place(x=160, y=1)
-counterLabel.configure(text='REPS')
-
-probLabel = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="black", padx=10)
-probLabel.place(x=300, y=1)
-probLabel.configure(text='PROB')
-
 classBox = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="blue")
 classBox.place(x=10, y=41)
 classBox.configure(text='0')
+'''
 
-counterBox = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="blue")
-counterBox.place(x=160, y=41)
-counterBox.configure(text='0')
-
-probBox = ck.CTkLabel(window, height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="blue")
-probBox.place(x=300, y=41)
-probBox.configure(text='0')
-
-# button
-button = ck.CTkButton(window, text='RESET', command=reset_counter, height=40, width=120, text_font=("Arial", 20),
+# buttons
+button_st = ck.CTkButton(window, text='Start', command=start, height=30, width=60, text_font=("Arial", 10),
                       text_color="white", fg_color="blue")
-button.place(x=10, y=600)
+button_st.place(x=740, y=240)
 
-# place to video
-frame = tk.Frame(height=480, width=480)
-frame.place(x=10, y=90)
-lmain = tk.Label(frame)
-lmain.place(x=0, y=0)
+button_stp = ck.CTkButton(window, text='End', command=end, height=30, width=60, text_font=("Arial", 10),
+                      text_color="white", fg_color="blue")
+button_stp.place(x=810, y=240)
+
+button_pus = ck.CTkButton(window, text='Pause', command=pause, height=30, width=60, text_font=("Arial", 10),
+                      text_color="white", fg_color="blue")
+button_pus.place(x=880, y=240)
+
+# place for videos
+frame1 = tk.Frame(height=480, width=660)
+frame1.place(x=20, y=60)
+lmain1 = tk.Label(frame1)
+lmain1.place(x=0, y=0)
+
+frame2 = tk.Frame(height=240, width=240)
+frame2.place(x=700, y=280)
+lmain2 = tk.Label(frame2)
+lmain2.place(x=0, y=0)
 
 # checkbox
-registered_label = tk.Label(window, text="Registration Status")  # lable
+skeleton_var = tk.StringVar(value="sk_off")
+skeleton_check = tk.Checkbutton(window, text="Skeleton View", variable=skeleton_var, onvalue="sk_on", offvalue="sk_off")
+skeleton_check.place(x=740, y=200)  # checkbox posission
 
-reg_status_var = tk.StringVar(value="Not Registered")
-registered_check = tk.Checkbutton(window, text="Currently Registered",
-                                  variable=reg_status_var, onvalue="Registered", offvalue="Not registered")
+blackBG_var = tk.StringVar(value="bbg_off")
+blackBG_check = tk.Checkbutton(window, text="Black Background", variable=blackBG_var, onvalue="bbg_on", offvalue="bbg_off")
+blackBG_check.place(x=740, y=180)
 
-registered_label.place(x=800, y=41)  # lable posision
-registered_check.place(x=800, y=60)  # checkbox posission
+cap = cv2.VideoCapture(0)
 
+
+def detect():
+    ret, frame = cap.read()
+
+    image1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image1 = cv2.resize(image1, (660,470))
+    img = image1[:, :650, :]
+    imgarr = Image.fromarray(img)
+    imgtk = ImageTk.PhotoImage(imgarr)
+    lmain1.imgtk = imgtk
+    lmain1.configure(image=imgtk)
+    lmain1.after(10, detect)
+
+
+detect()
 window.mainloop()
+
 
 print("Hello")
